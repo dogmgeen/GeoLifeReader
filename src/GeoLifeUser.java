7774f967194d431ea197dd4b2a3fe76aec142918
Doug McGeehan <djmvfb@mst.edu>
2015-01-22 20:41:34 -0600

    extracted per-file reading to its own function

--- src/GeoLifeUser.java
+++ src/GeoLifeUser.java
@@ -21,15 +21,11 @@ public class GeoLifeUser {
 	private double minY;
 	private double maxX;
 	private double maxY;
-	private boolean is_initialized;
 	
 	public GeoLifeUser(Path root, String userID) throws FileNotFoundException, ParseException {
 		this.userID = userID;
 		pltFiles = initPltFiles(root);
-		
-		// Initialize read values for later.
-		is_initialized = false;
-		
+				
 		minX = minY = Double.MAX_VALUE;	// All values are smaller than Double.MAX_VALUE.
 		maxX = maxY = Double.MIN_VALUE; // All values are bigger than Double.MIN_VALUE.
 		minTime = Integer.MAX_VALUE;
@@ -39,29 +35,7 @@ public class GeoLifeUser {
 	public void performAnalysis() throws FileNotFoundException, ParseException {
 		for (File f: pltFiles) {
 			System.out.println("User " + userID + " file " + f.getName());
-			// create a reader for the first file.
-			plt_scanner = new Scanner(f);
-			plt_scanner.nextLine();						//Geolife trajectory
-			plt_scanner.nextLine();						//WGS 84
-			plt_scanner.nextLine();						//Altitude is in Feet
-			plt_scanner.nextLine();						//Reserved 3
-			plt_scanner.nextLine();						//0,2,255,My Track,0,0,2,8421376
-			plt_scanner.nextLine();						//0
-			
-			GeoLifeRecord record = null;
-			while (plt_scanner.hasNext()) {
-				//39.984702,116.318417,0,492,39744.1201851852,2008-10-23,02:53:04
-				record = new GeoLifeRecord(plt_scanner.nextLine());
-				
-				minX = Math.min(record.x, minX);
-				maxX = Math.max(record.x, maxX);
-				minY = Math.min(record.y, minY);
-				maxY = Math.max(record.y, maxY);
-			}
-			// Once all files have been iterated, the remaining value in
-			//  epoch time is the max time.
-			maxTime = record.t;
-			plt_scanner.close();
+			analyzeFile(f);
 		}
 		
 		System.out.println("Statistics for GeoLife user " + userID);
@@ -76,6 +50,59 @@ public class GeoLifeUser {
 		System.out.printf("               %d months\n", (maxTime-minTime)/(3600*24*30));
 		System.out.printf("               %d years\n", (maxTime-minTime)/(3600*24*30*12));
 		System.out.println("====================================================");
+	}
+
+	private void analyzeFile(File f) throws FileNotFoundException, ParseException {
+		// create a reader for the first file.
+		Scanner s = new Scanner(f);
+		s.nextLine();						//Geolife trajectory
+		s.nextLine();						//WGS 84
+		s.nextLine();						//Altitude is in Feet
+		s.nextLine();						//Reserved 3
+		s.nextLine();						//0,2,255,My Track,0,0,2,8421376
+		s.nextLine();						//0
+		
+		GeoLifeRecord record = null;
+		double file_minX, file_minY, file_maxX, file_maxY;
+		int file_minTime, file_maxTime;
+		
+		file_minX = file_minY = Double.MAX_VALUE;	// All values are smaller than Double.MAX_VALUE.
+		file_maxX = file_maxY = Double.MIN_VALUE; // All values are bigger than Double.MIN_VALUE.
+		file_minTime = Integer.MAX_VALUE;
+		file_maxTime = Integer.MIN_VALUE;
+		
+		while (s.hasNext()) {
+			//39.984702,116.318417,0,492,39744.1201851852,2008-10-23,02:53:04
+			record = new GeoLifeRecord(s.nextLine());
+			
+			file_minX = Math.min(record.x, file_minX);
+			file_maxX = Math.max(record.x, file_maxX);
+			file_minY = Math.min(record.y, file_minY);
+			file_maxY = Math.max(record.y, file_maxY);
+			file_minTime = Math.min(record.t, file_minTime);
+			file_maxTime = Math.max(record.t, file_maxTime);
+		}
+		
+		System.out.printf("Min point: (%f, %f)\n", file_minX, file_minY);
+		System.out.printf("Max point: (%f, %f)\n", file_maxX, file_maxY);
+		System.out.printf("Map dimensions (meters): (%f, %f)\n", (file_maxX-file_minX), (file_maxY-file_minY));
+		System.out.printf("Min time: %d seconds\n", file_minTime);
+		System.out.printf("Max time: %d seconds\n", file_maxTime);
+		System.out.printf("Time duration: %d seconds\n", file_maxTime-file_minTime);
+		System.out.printf("               %d hours\n", (file_maxTime-file_minTime)/(3600));
+		System.out.printf("               %d days\n", (file_maxTime-file_minTime)/(3600*24));
+		System.out.printf("               %d months\n", (file_maxTime-file_minTime)/(3600*24*30));
+		System.out.printf("               %d years\n", (file_maxTime-file_minTime)/(3600*24*30*12));
+		System.out.println("====================================================");
+		
+		minX = Math.min(minX, file_minX);
+		minY = Math.min(minY, file_minY);
+		maxX = Math.max(maxX, file_maxX);
+		maxY = Math.max(maxY, file_maxY);
+		maxTime = Math.max(maxTime, file_maxTime);
+		minTime = Math.min(minTime, file_minTime);
+		
+		s.close();
 	}
 
 	private List<File> initPltFiles(Path root) {
