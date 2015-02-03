2e980adb4d2284718adfce579374b1e18e127f62
Doug McGeehan <djmvfb@mst.edu>
2015-02-03 10:48:09 -0600

    running all code

--- py/driver.py
+++ py/driver.py
@@ -8,21 +8,24 @@ logger = logging.getLogger("geolife")
 logger.addHandler(logging.StreamHandler())
 
 import geolife
+import sys
+sys.setrecursionlimit(100000)
 
 if __name__ == "__main__":
   try:
     sample_dir = "/home/djmvfb/Downloads/geo/"#"/home/kp/Development/GeoLifeReader/sample/"
     search_date = "2008-11-09"
+    num_users = 5
     geolife_root_directory = geolife.find_geolife_root(sample_dir)
     logger.info("GeoLife root found at {0}".format(geolife_root_directory))
 
     geolife.GeoLifeDataset(geolife_root_directory)\
-           .onlyRetrieveSomeUsers(n=5, randomize=False)
-    #       .retrieveByDate(date=search_date)\
-    #       .boundByLocation(north=53.567732, south=18.126, east=122.6, west=73.4)\
-    #       .calculateStatistics()\
-    #       .homogenizeTimeDeltas()\
-    #       .convertToONE(to_file="geolife2one_{0}.csv".format(search_date))
+           .onlyRetrieveSomeUsers(n=num_users)\
+           .retrieveByDate(date=search_date)\
+           .boundByLocation(north=53.567732, south=18.126, east=122.6, west=73.4)\
+           .calculateStatistics()\
+           .homogenizeTimeDeltas()\
+           .convertToONE(to_file="geolife2one_{0}_{1}users.csv".format(search_date, num_users))
 
   except:
     logger.exception("Stuff didn't do")
