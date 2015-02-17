627695e4a1c217e954fd3077da1935084829e2f1
Doug McGeehan <djmvfb@mst.edu>
2015-02-17 13:40:40 -0600

    dates successfully being generated

--- py/db_visualizer.py
+++ py/db_visualizer.py
@@ -1,14 +1,11 @@
 import logging
 logger = logging.getLogger("geolife.db_visualizer")
 
-from datetime import datetime
-from datetime import timedelta
 from record import GeoLifeRecord
-from utils import datetimerange
 
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy import func
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
   "{dialect}://{username}:{password}@{host}/{database}".format(
@@ -18,32 +15,13 @@ engine = create_engine(
   host='localhost',
   database='geolife'
 ))
-conn = engine.connect()
-"""
+
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
-# Determine the minimum and maximum dates in the database.
-beginning = session.query(GeoLifeRecord)\
-                   .order_by(GeoLifeRecord.datetime).first()
-end = session.query(GeoLifeRecord)\
-             .order_by(GeoLifeRecord.datetime.desc()).first()
-print(beginning.datetime, beginning.date)
-print(end.datetime, end.date)
-"""
-
-from sqlalchemy.sql import select
-
-stmt = select([GeoLifeRecord.date]).distinct().order_by(GeoLifeRecord.date)
-results = conn.execute(stmt)
-for r, in results:
-  print(r)
- 
-"""
-    #.with_hint(GeoLifeRecord, 'USE INDEX ix_records_date')\
-    self.result_set = self.result_set\
-        .filter(GeoLifeRecord.date==date)\
-        .order_by(GeoLifeRecord.datetime)
-"""
+results = session.query(func.count(GeoLifeRecord.date), GeoLifeRecord.date)\
+                 .group_by(GeoLifeRecord.date).order_by(GeoLifeRecord.date)
+for n, d in results:
+  print("{0};{1}".format(n, d))
 
