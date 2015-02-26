c94d3d6c789d469a2515c0d11c39311d70f34ade
Doug McGeehan <djmvfb@mst.edu>
2015-02-26 08:33:18 -0600

    Conversion through the MySQL database is horrendously slow. It is better to pull directly from the files

--- py/weekday.py
+++ py/weekday.py
@@ -7,7 +7,16 @@
 #  March 14th.
 
 import logging
+import sys
 logger = logging.getLogger("geolife.weekday")
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 from record import GeoLifeRecord
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
@@ -48,8 +57,33 @@ class WeekdayRecord(Base):
     )
 
 
+class TimingInfo:
+    def __init__(self, total_checkpoints):
+      self.start = datetime.datetime.now()
+      self.i = 0
+      self.n = total_checkpoints
+      self.avg = 0
+
+    def checkpoint(self):
+      duration = datetime.datetime.now() - self.start
+      seconds = duration.total_seconds()
+
+      self.avg += (seconds - self.avg)/(self.i + 1)
+      self.i += 1
+
+      self.start = datetime.datetime.now()
+      return duration
+
+    def eta(self):
+      remaining_checkpoints = self.n - self.i
+      eta_seconds = self.avg * remaining_checkpoints
+      eta = datetime.timedelta(seconds=eta_seconds)
+      return eta
+
+
 if __name__ == "__main__":
   # Load raw GeoLife records.
+  logger.info("Connecting to database")
   engine = create_engine(
       "{dialect}://{username}:{password}@{host}/{database}".format(
       dialect='mysql',
@@ -68,9 +102,9 @@ if __name__ == "__main__":
   ## DATA LOADING ##
   ##################
   #
-  original_data = session.query(GeoLifeRecord)
-  n = original_data.count()
-  print("There are {n} records in the GeoLifeRecord table".format(n=n))
+  logger.info("Counting records...")
+  n = session.query(GeoLifeRecord).count()
+  logger.info("There are {n} records in the GeoLifeRecord table".format(n=n))
 
   i = 0
   new_data = []
@@ -80,62 +114,57 @@ if __name__ == "__main__":
     "Origin date {0} is not a Monday".format(origin_date)
   )
 
-  for r in original_data:
-    datetime_suffix = r.datetime.strftime("%y%m%d")
-    new_user_id = int("{0}{1}".format(datetime_suffix, r.user))
+  DELTA_CHECKPOINT = 25000
+  timing_info = TimingInfo(total_checkpoints=int(float(n)/DELTA_CHECKPOINT))
 
-    days_after_origin_date = timedelta(days=r.weekday)
-    synthesized_date = origin_date + days_after_origin_date
-    synthesized_datetime = datetime.combine(synthesized_date, r.time)
-    assert synthesized_datetime.weekday() == r.weekday, (
-      "Synthesized datetime {0} does not fall on day {1}".format(
-        synthesized_datetime, WEEKDAY_VALUES[r.weekday]
-    ))
+  logger.info("Querying raw database for all GeoLifeRecords")
+  lastID = -1
+  query = session.query(GeoLifeRecord)
+  while True:
+    result = query.filter(GeoLifeRecord.id > lastID).limit(DELTA_CHECKPOINT).all()
+    if not result or len(result) == 0:
+      break
 
-    new_data.append(WeekdayRecord(
-      user=new_user_id,
-      latitude=r.latitude,
-      longitude=r.longitude,
-      datetime=synthesized_datetime,
-      weekday=r.weekday,
-    ))
+    for r in result:
+      lastID = r.id
 
-    another_5_percent_complete = (int(float(i)*100/n) % 5 == 0)
-    if another_5_percent_complete:
-      print("{0:.2%} complete ({1} out of {2} records)".format(
-        float(i)/n, i, n
+      datetime_suffix = r.datetime.strftime("%y%m%d")
+      new_user_id = int("{0}{1}".format(datetime_suffix, r.user))
+
+      weekday = r.date.weekday()
+      days_after_origin_date = datetime.timedelta(days=weekday)
+      synthesized_date = origin_date + days_after_origin_date
+      synthesized_datetime = datetime.datetime.combine(synthesized_date, r.time)
+      assert synthesized_datetime.weekday() == weekday, (
+        "Synthesized datetime {0} does not fall on day {1}".format(
+          synthesized_datetime, WEEKDAY_VALUES[weekday]
       ))
-      session.add_all(new_data)
-      session.commit()
-      del new_data[:]
 
-    i += 1
+      new_data.append(WeekdayRecord(
+        user=new_user_id,
+        latitude=r.latitude,
+        longitude=r.longitude,
+        datetime=synthesized_datetime,
+        weekday=weekday,
+      ))
 
-  print("Final committing of data ({0} records)".format(len(new_data)))
+      another_set_of_records_complete = (i % DELTA_CHECKPOINT == 0)
+      if another_set_of_records_complete:
+        logger.info("{0:.4%} complete ({1} out of {2} records)".format(
+          float(i)/n, i, n
+        ))
+        session.add_all(new_data)
+        session.commit()
+        del new_data[:]
+        duration = timing_info.checkpoint()
+        logger.info("{0} since last commit | ETA: {1}".format(
+          duration, timing_info.eta()
+        ))
+      i += 1
+
+  logger.info("Final committing of data ({0} records)".format(len(new_data)))
   session.add_all(new_data)
   session.commit()
   del new_data[:]
-  print("Data loading complete!")
-
-  #############################################################################
-  ## DATA VERIFYING ##
-  ####################
-  #
-  new_data = session.query(WeekdayRecord)
-  # Filter by each day.
-  for weekday in range(len(WEEKDAY_VALUES)):
-    # datetime's strptime format does not support ISO weekdays, but
-    #  instead uses 0 for Monday, 6 for Sunday. Thus, the weekday
-    #  values need to be decremented.
-    weekday_str = WEEKDAY_VALUES[weekday]
-    logger.info("Comparing size of data for {0}".format(weekday_str))
-
-    original_weekday_data = original_data.filter(GeoLifeRecord.weekday==weekday)
-    new_weekday_data = new_data.filter(WeekdayRecord.weekday==weekday)
-    original_data_size = original_data.count()
-    new_data_size = new_weekday_data.count()
-    assert original_data_size == new_data_size, (
-      "Original data has {0} records for {1}, but new data has {2}"
-      " records".format(original_data_size, weekday_str, new_data_size)
-    )
+  logger.info("Data loading complete!")
 
