4e8bc7c1a2f25aee28c002666135946b0db758a0
Doug McGeehan <djmvfb@mst.edu>
2015-03-04 12:01:43 -0600

    initial time_homogenize code

---
+++ py/time_homogenize.py
@@ -0,0 +1,87 @@
+import logging
+logger = logging.getLogger("geolife.record")
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import BigInteger
+from sqlalchemy import Float
+from sqlalchemy import Time
+from sqlalchemy import Sequence
+from sqlalchemy import SmallInteger
+from datetime import timedelta
+from datetime import time
+from utils import datetimerange
+from raw.record import GeoLifeUser
+
+WEEKDAY_STRINGS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+]
+
+Base = declarative_base()
+class HomogenizedRecord(Base):
+  __tablename__ = "time_homogenized"
+  id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+  user = Column(BigInteger)#, index=True)
+  latitude = Column(Float)
+  longitude = Column(Float)
+  time = Column(Time)
+  weekday = Column(SmallInteger)
+
+  MONDAY = 0
+  TUESDAY = 1
+  WEDNESDAY = 2
+  THURSDAY = 3
+  FRIDAY = 4
+  SATURDAY = 5
+  SUNDAY = 6
+  WEEKDAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY]
+
+  def __repr__(self):
+    return "<WeekdayRecord(name={0}, (x,y)=({1}, {2}), datetime={3})>".format(
+      self.user, self.latitude, self.longitude,
+      self.datetime
+    )
+
+class HomogenizedGeoLifeUser(Base):
+  __tablename__ = "time_homo_user_weekday_count"
+  id = Column(BigInteger, primary_key=True)
+  count = Column(Integer)
+
+
+def initialize_table(engine):
+  Base.metadata.create_all(engine)
+
+
+
+if __name__ == "__main__":
+  for weekday in HomogenizedRecord.WEEKDAYS:
+    print(weekday)
+    """
+    users = get_users_present_on(weekday)
+    users_present = []
+    homogenized_records = []
+    for t in datetimerange(time.min, tim.max, delta):
+      records = SELECT * FROM records
+              WHERE datetime in (t, t+delta)
+      for r in records:
+        users_present.append(r.user)
+        r = modify_record_timestamp(r, t)
+        homogenized_records.append(r)
+
+      users_not_present = users - users_present
+      for u in users_not_present:
+        most_recent_record = most_recent_user_record_before(t, u)
+        r = modify_record_timestamp(most_recent_record, t)
+        homogenized_records.append(r)
+
+      insert_into_time_homogenized_database(homogenized_records)
+
+      del users_present[:]
+      del homogenized_records[:]
+    """
