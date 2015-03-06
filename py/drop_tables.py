0057b90448fbe88b2e5c673ed6252e7cc4129ec5
Doug McGeehan <djmvfb@mst.edu>
2015-03-06 11:21:32 -0600

    fixed type in import

--- py/drop_tables.py
+++ py/drop_tables.py
@@ -3,9 +3,9 @@ from raw.record import WRecord
 from sqlalchemy import create_engine
 from schema import HomogenizedGeoLifeUser
 from schema import HomogenizedRecord
-from config import getEngine()
+from config import getEngine
 
-engine = config.getEngine()
+engine = getEngine()
 GeoLifeUser.__table__.drop(engine, checkfirst=True)
 WRecord.__table__.drop(engine, checkfirst=True)
 HomogenizedRecord.__table__.drop(engine, checkfirst=True)
