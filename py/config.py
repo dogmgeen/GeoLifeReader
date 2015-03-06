20464b66834c149b6908a9903f0a9232603ae870
Doug McGeehan <djmvfb@mst.edu>
2015-03-06 11:18:14 -0600

    migrated engine creation to config file

--- py/config.py
+++ py/config.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from sqlalchemy import create_engine
 
 ALL_CHINA = {
   "north": 53.567732,
@@ -14,9 +15,19 @@ BEIJING = {
   "west": 115.3983897,
 }
 
-TRACE_DESTINATION_DIRECTORY = "/home/djmvfb/Development/chitchat/traces/geolife"
+TRACE_DESTINATION_DIRECTORY = "/home/djmvfb/one"
 TRACE_FILENAME_FORMAT = "geolife2one_{0}_{1}users.csv"
 DELTA = timedelta(seconds=5)
 NUM_MESSAGES = 5000
 NUM_USERS = 20
+
+def getEngine():
+  return create_engine(
+    "{dialect}://{username}:{password}@{host}/{database}".format(
+    dialect='postgresql+psycopg2',
+    username='postgres',
+    password='nope',
+    host='localhost',
+    database='geolife'
+  ))
 
