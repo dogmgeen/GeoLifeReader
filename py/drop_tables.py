cdeaff55afdd841330a65f9194a798b9f078f1d0
Doug McGeehan <djmvfb@mst.edu>
2015-09-05 14:51:57 -0500

    some tables are no longer used

--- py/drop_tables.py
+++ py/drop_tables.py
@@ -1,12 +1,8 @@
 from raw.record import GeoLifeUser
-from raw.record import WRecord
+from raw.record import RawRecord
 from sqlalchemy import create_engine
-from schema import HomogenizedGeoLifeUser
-from schema import HomogenizedRecord
 from config import getEngine
 
 engine = getEngine()
 GeoLifeUser.__table__.drop(engine, checkfirst=True)
-WRecord.__table__.drop(engine, checkfirst=True)
-HomogenizedRecord.__table__.drop(engine, checkfirst=True)
-HomogenizedGeoLifeUser.__table__.drop(engine, checkfirst=True)
+RawRecord.__table__.drop(engine, checkfirst=True)
