55cd4bc16f4e8b963ab558bc9952dad15a620bfb
Doug McGeehan <djmvfb@mst.edu>
2015-12-13 12:42:28 -0600

    calculating unique users and records per day

--- py/raw_stats.py
+++ py/raw_stats.py
@@ -7,38 +7,56 @@ logger = logging.getLogger("geolife")
 stdout = logging.StreamHandler()
 stdout.setLevel(logging.DEBUG)
 logger.addHandler(stdout)
-
-import config
-engine = config.getEngine()
-
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker()
-Session.configure(bind=engine)
-
-
-from raw.record import RawRecord
+import os
+import geolife
+from raw import user
 from collections import defaultdict
 from utils import ETACalculator
+import argparse
+# Parse the command-line arguments.
+def get_arguments():
+  parser = argparse.ArgumentParser(
+    description='Populate local database with each raw trajectory.'
+  )
+  parser.add_argument(
+    '-i', '--input_directory',
+    dest="input_directory",
+    help='Directory containing PLT files (default: current working directory)',
+    type=geolife.find_geolife_root,
+    required=True,
+  )
+
+  args = parser.parse_args()
+  return args
+
+
+
+
 if __name__ == "__main__":
-  session = Session()
-  records = session.query(RawRecord)
-  print("Counting number of records...")
-  n = records.count()
-  print("{0} records to iterate over".format(n))
-  eta = ETACalculator(n, name="Record iteration")
-  print("="*80)
+  args = get_arguments()
+  directory = args.input_directory
+  eta = ETACalculator(iterations=geolife.get_num_files(directory))
+
   unique_users_per_day = defaultdict(set)
   unique_records_per_day = defaultdict(int)
-  for r in records.all():
-    user_id = r.user
-    date = r.date
+  for u in user.from_directory(directory):
 
-    # Add the user id to the set of unique users per day.
-    unique_users_per_day[date].add(user_id)
-    unique_records_per_day[date] += 1
-    eta.checkpoint()
-    print(eta.eta())
-  
+    logger.info("Beginning yielding of records from user {0.id}".format(u))
+    for f in u.files:
+      for r in f:
+        user_id = r.user
+        date = r.date
+
+        # Add the user id to the set of unique users per day.
+        unique_users_per_day[date].add(user_id)
+        unique_records_per_day[date] += 1
+
+      eta.checkpoint()
+      logger.info("="*60)
+      logger.info("File {0}".format(os.path.basename(f.url)))
+      logger.info(eta.eta())
+      u.num_records += f.num_records
+
   print("Number of unique users per day:")
   with open("unique_users_per_day.csv", "w") as outfile:
     for d in unique_users_per_day:
