bc9545ec1a05b11fa87f512f2c311b16881c4de1
Doug McGeehan <djmvfb@mst.edu>
2018-01-28 12:40:57 -0600

    I thought I fixed the problem of the progress bar, but I guessed not

--- time_homogenize.py
+++ time_homogenize.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
   homogenized_records = []
 
   n = num_elements_in_time_range(start=time.min, end=time.max, step=delta)
-  with progressbar.ProgressBar(max_val=n) as progress:
+  with progressbar.ProgressBar(max_value=n) as progress:
 
     for index, t in enumerate(timerange(time.min, time.max, delta)):
         logger.debug("="*60)
